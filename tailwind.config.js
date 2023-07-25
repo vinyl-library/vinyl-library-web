0ce6a8ba1ea2363cc6bc4d40c1800b1cbf81a356
Aidah Novallia Putri <aidahptr15@gmail.com>
2023-07-25 20:19:46 +0700

    add color and font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'powder': '#F8F8F0',
+      'tiger': '#BC6C25',
+      'buff': '#D4A373',
+      'crayola': '#FB5770',
+      'bean':'#3B1500',
+      'lace': '#F7F3E6',
+      'alabaster': '#EAE6D9'
+    },
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+      mulish: ['Mulish', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
