16164662ef5100c130400b819c6cf33041750118
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-07-27 13:30:40 +0700

    prettier

--- src/components/elements/ProductSide/index.tsx
+++ src/components/elements/ProductSide/index.tsx
@@ -4,9 +4,9 @@ import { ProductSideProps } from './interface'
 
 const ProductSide: React.FC<ProductSideProps> = ({ imageUrl }) => {
   return (
-    <div className="w-full lg:w-1/2 h-28 lg:h-full mt-32 lg:mt-0 lg:bg-buff lg:bg-opacity-10 rounded-l-xl flex relative order-2 lg:order-1">
+    <div className="w-full group lg:w-1/2 h-28 lg:h-full mt-32 lg:mt-0 lg:bg-buff lg:bg-opacity-10 rounded-l-xl flex relative order-2 lg:order-1">
       <div className="text-center hidden lg:flex items-center justify-start h-full w-full select-none">
-        <span className="transform whitespace-nowrap h-full -rotate-90 text-[55px] 2xl:text-[70px] font-black uppercase text-crayola opacity-0 transition-all group-hover:opacity-60 ml-10 lg:ml-12 2xl:ml-32 group-hover:-ml-20 2xl:group-hover:ml-36 lg:group-hover:ml-20 duration-1000 lg:duration-700 ease-in-out">
+        <span className="transform whitespace-nowrap h-full -rotate-90 text-[55px] 2xl:text-[64px] font-black uppercase text-crayola opacity-0 transition-all group-hover:opacity-60 ml-10 lg:ml-12 2xl:ml-18 group-hover:-ml-20 lg:group-hover:ml-20 duration-1000 lg:duration-700 ease-in-out">
           vinyl library
         </span>
       </div>
@@ -18,7 +18,7 @@ const ProductSide: React.FC<ProductSideProps> = ({ imageUrl }) => {
           width={0}
           height={0}
           sizes="100vw"
-          className="-mb-5 lg:mb-0 -ml-12 lg:ml-0 h-[500px] xl:h-[700px] 2xl:h-[800px] w-auto object-cover transform group-hover:translate-x-20 2xl:group-hover:translate-x-30 transition-all duration-1000 lg:duration-700 ease-in-out"
+          className="-mb-5 lg:mb-0 -ml-12 lg:ml-0 h-[30rem] xl:h-[40rem] 2xl:h-[44rem] w-auto object-cover transform group-hover:translate-x-20 2xl:group-hover:translate-x-30 transition-all duration-1000 lg:duration-700 ease-in-out"
         />
         {/* <div className="shadow w-full h-8 bg-tiger bg-opacity-20 filter blur absolute bottom-0 md:bottom-12 lg:bottom-18 xl:bottom-24 2xl:bottom-28 left-0 lg:left-24 rounded-full transform skew-x-20"></div> */}
       </div>
