3b590ee8e3148c911c6d1461539c281a660663ae
aidahputri <aidahptr15@gmail.com>
2023-07-28 17:19:30 +0700

    init layout page

---
+++ src/components/modules/LayoutModule/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import {HeroSection, FAQSection} from './sections
+// import {} from './module-elements'
+
+export const LayoutModule: React.FC = () => {
+  // TODO: Write module's logic
+
+  return (
+    <>
+      {/* <HeroSection></HeroSection> */}
+      {/* <FAQSection></FAQSection> */}
+    </>
+  )
+}
