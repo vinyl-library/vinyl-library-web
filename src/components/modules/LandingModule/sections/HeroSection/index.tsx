987b59ca5a7d50c168a972966bdd8eee6e896897
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-08-02 18:14:34 +0700

    add package

--- src/components/modules/LandingModule/sections/HeroSection/index.tsx
+++ src/components/modules/LandingModule/sections/HeroSection/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Image from 'next/image'
 
 export const HeroSection: React.FC = () => {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
   return (
     <section className="py-12">
-      <div className="container mx-auto h-full relative">
-        <div className="flex flex-col gap-y-4 md:gap-0 xl:flex-row items-center px-8 3xl:px-12 py-24 3xl:py-28 justify-between h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col gap-y-4 md:gap-0 xl:flex-row px-8 3xl:px-12 py-24 3xl:py-28 justify-between h-full">
           <div className="flex flex-col gap-y-4 pl-8">
-            <div className="flex flex-col gap-y-2 font-extrabold text-[36px] lg:text-[48px] 3xl:text-[60px] xl:absolute">
+            <div className="flex flex-col gap-y-1 font-extrabold text-[36px] lg:text-[48px] 3xl:text-[60px]">
               <h1
                 className="text-crayola"
                 data-aos="fade-down"
@@ -24,7 +30,7 @@ export const HeroSection: React.FC = () => {
               </h1>
             </div>
 
-            <div className="xl:absolute flex">
+            <div className="flex">
               <p
                 className="max-w-[90%] text-bean text-[10px] lg:text-[16px] 3xl:text-[24px]"
                 data-aos="fade-down"
@@ -38,17 +44,17 @@ export const HeroSection: React.FC = () => {
           </div>
 
           <div
-            className="xl:absolute xl:-right-12 xl:bottom-16"
+            className="flex justify-center items-start h-full w-full"
             data-aos="fade-up"
             data-aos-delay="700"
           >
             <Image
               alt="Hero image"
               src="/Hero.png"
-              width={0}
-              height={0}
+              width={600}
+              height={600}
               sizes="100vw"
-              className="h-[40rem]"
+              className="object-cover"
             />
           </div>
         </div>
