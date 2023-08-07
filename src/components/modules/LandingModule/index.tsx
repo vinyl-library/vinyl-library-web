ff7ce59a99718d43cc3f46d7c634c05b5840e1c0
aidahputri <aidahptr15@gmail.com>
2023-08-07 16:14:33 +0700

    prettier

--- src/components/modules/LandingModule/index.tsx
+++ src/components/modules/LandingModule/index.tsx
@@ -6,7 +6,7 @@ export const LandingModule: React.FC = () => {
     <>
       <HeroSection />
       <MostPopularSection />
-      <RecommendedForYouSection/>
+      <RecommendedForYouSection />
     </>
   )
 }
