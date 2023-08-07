851f0e806f0afe97c8cad7939bf663831425c293
aidahputri <aidahptr15@gmail.com>
2023-08-07 16:13:50 +0700

    complete recommended for you section

--- src/components/modules/LandingModule/index.tsx
+++ src/components/modules/LandingModule/index.tsx
@@ -1,10 +1,12 @@
 import { HeroSection, MostPopularSection } from './sections'
+import { RecommendedForYouSection } from './sections/RecommendedForYouSection'
 
 export const LandingModule: React.FC = () => {
   return (
     <>
       <HeroSection />
       <MostPopularSection />
+      <RecommendedForYouSection/>
     </>
   )
 }
