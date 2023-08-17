e1c16ea3bb23a839123754b16b44a98f08946a0c
Aidah Novallia Putri <aidahptr15@gmail.com>
2023-08-17 14:18:43 +0700

    improve select element styling

--- src/components/elements/Select/index.tsx
+++ src/components/elements/Select/index.tsx
@@ -12,72 +12,57 @@ export const Select: React.FC<SelectProps> = ({
 
   useEffect(() => {
     selectedValue && onChange(selectedValue)
+    setIsDropdownOpen(false)
   }, [selectedValue])
 
   return (
-    <>
-      {/* <div className="text-black">
-        SELECT
-        <div>
-          {options.map((option) => {
-            return (
-              <div onClick={() => setSelectedValue(option.value)}>
-                {option.name}
-              </div>
-            )
-          })}
-        </div>
-      </div> */}
-
-      <div className="relative text-left font-semibold">
+    <div className="relative text-left font-semibold">
+      <div
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        className="cursor-pointer rounded-lg bg-crayola px-4 py-2 text-md font-medium hover:bg-opacity-90 flex items-center justify-between"
+      >
+        <span className="pr-6">{selectedOption}</span>
         <div
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          className="cursor-pointer rounded-lg bg-crayola px-4 py-2 text-md font-medium hover:bg-opacity-90 flex items-center justify-between"
-        >
-          <span className="pr-6">{selectedOption}</span>
-          <div
-            className={`transform transition-transform ${
-              isDropdownOpen ? 'rotate-180' : ''
+          className={`transform transition-transform ${isDropdownOpen ? 'rotate-180' : ''
             }`}
+        >
+          <svg
+            className="w-4 h-4 ml-2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            <svg
-              className="w-4 h-4 ml-2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M19 9l-7 7-7-7"></path>
-            </svg>
+            <path d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </div>
+      </div>
+      {isDropdownOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 xl:w-64 rounded-md shadow-lg bg-crayola ring-1 ring-opacity-5">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            {options.map((option, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  setSelectedValue(option.value)
+                  setSelectedOption(option.name)
+                }}
+                className="block w-full text-left px-4 py-2 text-md hover:bg-white/20 hover:text-white/80"
+                role="menuitem"
+              >
+                {option.name}
+              </button>
+            ))}
           </div>
         </div>
-        {isDropdownOpen && (
-          <div className="origin-top-right absolute right-0 mt-2 w-56 xl:w-64 rounded-md shadow-lg bg-crayola ring-1 ring-opacity-5">
-            <div
-              className="py-1"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="options-menu"
-            >
-              {options.map((option, index) => (
-                <button
-                  key={index}
-                  onClick={() => {
-                    setSelectedValue(option.value)
-                    setSelectedOption(option.name)
-                  }}
-                  className="block w-full text-left px-4 py-2 text-md hover:bg-white/20 hover:text-white/80"
-                  role="menuitem"
-                >
-                  {option.name}
-                </button>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    </>
+      )}
+    </div>
   )
 }
